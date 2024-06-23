bdda0bae8cabcfeefb8fd0d01ed9c2e0f04629bb
Jason Warner <xocomil@gmail.com>
2024-06-23 13:16:51 -0600

    add padding and details

--- apps/modular-state/src/app/app.component.ts
+++ apps/modular-state/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { VehicleDetailsComponent } from '@modular-state/vehicle.details';
 import { VehicleInfoComponent } from '@modular-state/vehicle.info';
 
 @Component({
   standalone: true,
-  imports: [VehicleInfoComponent, FormsModule],
+  imports: [VehicleInfoComponent, FormsModule, VehicleDetailsComponent],
   selector: 'app-root',
   template: `
-    <div class="container">
+    <div class="container p-2">
       <form>
         <mod-state-vehicle-info />
+        <mod-state-vehicle-details />
       </form>
     </div>
   `,

760f3049d56e98cac88cde25330e014c020eee97
Jason Warner <xocomil@gmail.com>
2024-06-27 17:59:09 -0600

    added default routes

--- apps/modular-state/src/app/app.routes.ts
+++ apps/modular-state/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('@modular-state/combined').then((m) => m.CombinedComponent),
+  },
+  {
+    path: '*',
+    redirectTo: '',
+  },
+];

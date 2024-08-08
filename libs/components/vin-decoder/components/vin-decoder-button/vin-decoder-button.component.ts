7b54fa340013167cf69aff58a5ac8784f378bc13
Jason Warner <xocomil@gmail.com>
2024-08-08 17:53:17 -0600

    added `VinDecoderButtonComponent`

--- libs/components/vin-decoder/components/vin-decoder-button/vin-decoder-button.component.ts
+++ libs/components/vin-decoder/components/vin-decoder-button/vin-decoder-button.component.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'mod-state-vin-decoder',
+  selector: 'mod-state-vin-decoder-button',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>vin-decoder works!</p>`,
-  styleUrl: './vin-decoder.component.css',
+  template: `<p>vin-decoder-button works!</p>`,
+  styleUrl: './vin-decoder-button.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VinDecoderComponent {}
+export class VinDecoderButtonComponent {}
